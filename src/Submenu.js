bdede5e4b772784aa8f3cf8cbde3b5d1573dc74b
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-04-15 15:44:33 +0100

    sidebar and hero has been added

--- src/Submenu.js
+++ src/Submenu.js
@@ -1,7 +1,11 @@
 import React, { useState, useRef, useEffect } from 'react'
-
+import {useGlobalContext} from './context'
 const Submenu = () => {
-  return <h2>submenu component</h2>
+  const {isSubMenuOpen}=useGlobalContext()
+
+  return <aside className={`submenu ${isSubMenuOpen&&'show'}`}>
+    subMenu
+  </aside>
 }
 
 export default Submenu
--- src/context.js
+++ src/context.js
@@ -1,2 +1,31 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+export const AppContext=React.createContext()
+
+export const AppProvider=({children})=>{
+    const[isSidebarOpen,setIsSidebarOpen]=useState(false)
+    const[isSubMenuOpen,setIsSubMenuOpen]=useState(true)
+
+    const openSidebar=()=>{
+        setIsSidebarOpen(true)
+    }
+    const closeSidebar=()=>{
+        setIsSidebarOpen(false)
+    }
+    const openSubMenu=()=>{
+        setIsSidebarOpen(true)
+    }
+    const closeSubMenu=()=>{
+        setIsSubMenuOpen(false)
+    }
+    return <AppContext.Provider value={{
+        isSubMenuOpen,
+        isSidebarOpen,setIsSubMenuOpen,setIsSidebarOpen,openSubMenu,openSidebar,closeSubMenu,closeSidebar}} >
+        {children}
+    </AppContext.Provider>
+}
+
+export const useGlobalContext=()=>{
+    return useContext(AppContext)
+}
