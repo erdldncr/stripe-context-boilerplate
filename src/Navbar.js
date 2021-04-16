81edb3fa84e232ef2705417cbef307a16298a4e9
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-04-16 06:19:29 +0100

    project completed

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import logo from './images/logo.svg'
 import { FaBars } from 'react-icons/fa'
 import {useGlobalContext} from './context'
 const Navbar = () => {
   const{openSidebar,openSubMenu,closeSubMenu}=useGlobalContext()
   
-  return <nav className="nav">
+  const displaySubMenu=(e)=>{
+    const page=e.target.textContent
+    const tempBtn=e.target.getBoundingClientRect()
+    const center=(tempBtn.left+tempBtn.right)/2
+    const bottom=tempBtn.bottom-3
+    openSubMenu(page,{center,bottom})
+  }
+  const handleSubMenu=(e)=>{
+    if(!e.target.classList.contains('link-btn')){
+      closeSubMenu()
+    }
+  }
+  
+  return <nav className="nav" onMouseOver={handleSubMenu} >
     <div className="nav-center">
       <div className="nav-header">
         <img src={logo} alt='coding' className='nav-logo'/>
@@ -15,16 +28,16 @@ const Navbar = () => {
       </div>
       <ul className="nav-links">
         <li>
-          <button className="link-btn">products</button>
+          <button className="link-btn"  onMouseOver={displaySubMenu}  >products</button>
         </li>
         <li>
-          <button className="link-btn">developers</button>
+          <button className="link-btn" onMouseOver={displaySubMenu} >developers</button>
         </li>
         <li>
-          <button className="link-btn">company</button>
+          <button className="link-btn" onMouseOver={displaySubMenu} >company</button>
         </li>
       </ul>
-      <button className="btn signin-btn">Sign in</button>
+      <button className="btn signin-btn" onMouseOver={displaySubMenu} >Sign in</button>
     </div>
   </nav>
 }
